5511d5f943b350e7901a936cdea0e16010f114ed
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-08-05 12:26:06 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,59 @@
+// 
+// If Jacdac Button 1 is pressed down send 1 to receiver
+// 
+// This value determines direction of Motor
+modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
+    radio.sendNumber(1)
+})
+/**
+ * Jacdac Motor Remote Control Transmitter
+ * 
+ * Micro:Bit V2 & Jacadaptor
+ * 
+ * Uses 2 Jacdac Keycap Button modules and Jacdac Slide Potentiometer 
+ * 
+ * Buttons are used to transmit the desired direction of the Motor connected to the Receiver
+ * 
+ * Slider Potentiometer is used to send the desired motor seep to the Receiver
+ */
+modules.rotaryEncoder1.onPositionChanged(function () {
+    ROTOR = modules.rotaryEncoder1.position()
+    basic.showNumber(ROTOR)
+    if (ROTOR >= 1) {
+        radio.sendString("a")
+    } else if (ROTOR < 0) {
+        radio.sendString("b")
+    }
+})
+// 
+// If Jacdac Button 2 is pressed down send -1 to receiver
+// 
+// This value determines direction of Motor
+modules.button2.onEvent(jacdac.ButtonEvent.Down, function () {
+    radio.sendNumber(-1)
+})
+// Show Icon on Micro:Bit to show system is ready
+// 
+// Set radio Group to match that of receiver
+// 
+// Set Variavle "Speed" to 0 - this is used to control the speed of the motor connected to the receiver
+// 
+// 
+let ROTOR = 0
+radio.setGroup(1)
+radio.setTransmitPower(7)
+let Speed = 0
+led.setBrightness(107)
+basic.showString("T")
+// Set Variable "Speed" to value of device used to control speed of motor.
+// 
+// Send this value via radio with a name of "Speed" 
+// 
+// In this case we are using a Jacdac Slide Potentiometer 
+// 
+// Other devices that give an analog output ( rotary Potentiometer etc ) 
+// could also be used
+basic.forever(function () {
+    Speed = Math.ceil(modules.potentiometer1.position())
+    radio.sendValue("Speed", Speed)
+})
